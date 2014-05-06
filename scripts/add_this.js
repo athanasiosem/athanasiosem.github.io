c557f103b7ea868ff90923b0d84e8ab62d0c4869
Athanasios Emmanouilidis <athanasiosem@inbox.com>
2014-05-06 14:46:06 +0300

    changed add_this code

--- scripts/add_this.js
+++ scripts/add_this.js
@@ -1,7 +1,4 @@
-<!-- AddThis Smart Layers BEGIN -->
-<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f5386f42f3facd"></script>
-<script type="text/javascript">
+// Go to http://www.addthis.com/get/smart-layers to customize
   addthis.layers({
     'theme' : 'transparent',
     'share' : {
@@ -16,5 +13,3 @@
       ]
     }   
   });
-</script>
-<!-- AddThis Smart Layers END -->
