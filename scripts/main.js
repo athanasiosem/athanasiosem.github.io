5d4ec7d85448c414b86ef48a18cb6885172f09eb
Athanasios Emmanouilidis <athanasiosem@inbox.com>
2015-06-23 14:15:02 +0300

    fixed javascript code

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,6 @@
 
             $('#home').show();
             $('#about').hide();
-            $('#cv').hide();
             $('#contact').hide();
 
             // Remove active class from all other navigation items and add it to this item
@@ -29,12 +28,11 @@
 
             $('#home').slideDown();
             $('#about').slideUp();
-            $('#cv').slideUp();
             $('#contact').slideUp();
 
             // Remove active class from all other navigation items and add it to this item
 
-            $('#about-nav-li,#cv-nav-li,#contact-nav-li').removeClass("active");
+            $('#about-nav-li,#contact-nav-li').removeClass("active");
             $('#home-nav-li').addClass("active");
 
         });
@@ -45,32 +43,16 @@
 
             $('#home').slideUp();
             $('#about').slideDown();
-            $('#cv').slideUp();
             $('#contact').slideUp();
 
             // Remove active class from all other navigation items and add it to this item
 
-            $('#home-nav-li,#cv-nav-li,#contact-nav-li').removeClass("active");
+            $('#home-nav-li,#contact-nav-li').removeClass("active");
             $('#about-nav-li').addClass("active");
 
         });
 
-        this.get('#/cv', function (context) {
-
-            // Hide all other pages and show only this page
-
-            $('#home').slideUp();
-            $('#about').slideUp();
-            $('#cv').slideDown();
-            $('#contact').slideUp();
-
-            // Remove active class from all other navigation items and add it to this item
-
-            $('#home-nav-li,#about-nav-li,#contact-nav-li').removeClass("active");
-            $('#cv-nav-li').addClass("active");
-
-        });
-
+        
 
         this.get('#/contact', function (context) {
 
@@ -78,13 +60,12 @@
 
             $('#home').slideUp();
             $('#about').slideUp();
-            $('#cv').slideUp();
             $('#contact').slideDown();
 
 
             // Remove active class from all other navigation items and add it to this item
 
-            $('#home-nav-li,#about-nav-li,#cv-nav-li').removeClass("active");
+            $('#home-nav-li,#about-nav-li').removeClass("active");
             $('#contact-nav-li').addClass("active");
 
         });
